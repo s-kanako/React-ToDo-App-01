2081620a6a1e806bf5e40b32c40b3c57ff6e2884
かなこ <s.ka10na26ko@gmail.com>
2020-08-02 23:11:23 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
+import './App.css';
+
+class App extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            list: ['プログラミング'],
+            value: '',
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            list :[
+                ...this.state.list,
+                this.state.value,
+            ],
+            value: '',
+        });
+    }
+    render () {
+        return (
+            <div className="app">
+                <h1>TODO　アプリ</h1>
+                <Form 
+                    value={this.state.value}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                />
+                <TodoList list={this.state.list}/>
+            </div>
+        );
+    }
+}
+
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+
+
+function TodoList(props) {
+    return (
+        <List>
+            {
+                props.list.map((item, i) =>  (
+                    <ListItem key={i}>
+                        <ListItemText primary={item}　/>
+                    </ListItem>     
+                ))
+            }
+        </List>
+    );
+}
+
+export default TodoList;
